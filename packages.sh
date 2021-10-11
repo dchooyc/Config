2a7d11e00e135e5dda5ce560bd8dcf4274900b30
dchooyc <dchooyc@gmail.com>
2021-10-11 17:49:10 +0800

    Refactor packages for readability

--- packages.sh
+++ packages.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
+# This installs arch packages
 
-sudo pacman -S python python-pip go
+# Set colour for echo
+green='\033[0;32m'
+
+# Python related packages
+echo -e "\n${green}Installing python related packages\n"
+sudo pacman -S\
+ python python-pip
+
+# Golang related packages
+echo -e "\n${green}Installing golang related packages\n"
+sudo pacman -S\
+ go
