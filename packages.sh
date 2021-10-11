ebef42f0c52d30bbf18f4d42c2ba50ddcbcb723b
dchooyc <dchooyc@gmail.com>
2021-10-11 18:53:39 +0800

    Add no colour and install with pip

--- packages.sh
+++ packages.sh
@@ -3,13 +3,19 @@
 
 # Set colour for echo
 green='\033[0;32m'
+nc='\033[0m'
 
 # Python related packages
-echo -e "\n${green}Installing python related packages\n"
+echo -e "\n${green}Installing python related packages${nc}\n"
 sudo pacman -S\
- python python-pip
+ python python-pip python-mysqlclient
+
+# Python installs with pip
+echo -e "\n${green}Installing python stuff with pip${nc}\n"
+python -m pip install\
+ Django
 
 # Golang related packages
-echo -e "\n${green}Installing golang related packages\n"
+echo -e "\n${green}Installing golang related packages${nc}\n"
 sudo pacman -S\
  go
